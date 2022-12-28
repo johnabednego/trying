cf48c5ee22a3a0ed3b13da36683e9268e0000b89
JOHN ABEDNEGO JILIMA <jajilima@st.ug.edu.gh>
2022-12-28 14:35:48 +0000

    chrome

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -538,16 +538,16 @@ class Video extends Component {
 	}
 
 	render() {
-		if (this.isChrome() === false) {
-			return (
-				<div style={{
-					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
-					textAlign: "center", margin: "auto", marginTop: "50px", justifyContent: "center"
-				}}>
-					<h1>Sorry, this works only with Google Chrome</h1>
-				</div>
-			)
-		}
+		// if (this.isChrome() === false) {
+		// 	return (
+		// 		<div style={{
+		// 			background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
+		// 			textAlign: "center", margin: "auto", marginTop: "50px", justifyContent: "center"
+		// 		}}>
+		// 			<h1>Sorry, this works only with Google Chrome</h1>
+		// 		</div>
+		// 	)
+		// }
 
 		if (this.state.message !== "") {
 			let messageInput = document.getElementById("messageInput");
